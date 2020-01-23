aeaebaef696c6eca065832e4dd3bd49ae206e55e
mateo pèrez <yta123@gmail.com>
2020-01-22 22:38:28 -0500

    first version of project
    I consume an endpoint for search job opportunities by position or
    company name, I show them in a card list with details, the user can
    choose how many offers want to get
    	modified:   README.md
    	modified:   src/index.html
    	new file:   src/job.html
    	new file:   src/profile.html
    	modified:   src/public/js/api.js

--- README.md
+++ README.md
@@ -21,3 +21,7 @@ I create a project, I will do it in javascript pure.
 > 01/22/2020 6:52 pm
 
 I do the first scratch of the website with a very basic example of the endpoint search
+
+> 01/22/2020 10:27 pm
+
+I finish the first version of my project, I consume the endpoint for search companies by position and company name. I show the results in a card list with details of job offer.
--- src/public/js/api.js
+++ src/public/js/api.js
@@ -1,12 +1,13 @@
 
 function searchFor(){
-    var value =  document.getElementById('searchfor').value;
-    consumeapi(value);
+    let value =  document.getElementById('searchfor').value;
+    let size = document.getElementById('size').value;
+    consumeapi(value,size);
     return false;
  }
 
- function consumeapi(value){
-    fetch(`https://search.torre.co/opportunities/_search/?offset=1&size=20&remote=false`,
+ function consumeapi(value,size){
+    fetch(`https://search.torre.co/opportunities/_search/?offset=1&size=${size}`,
     {
         method: 'POST',
         headers: {
@@ -19,17 +20,61 @@ function searchFor(){
     })
     .then(function(res) {
       
-        addjobs(res.results);
+        addjobs(res.results,value);
     });
  }
 
 
-  function addjobs(data){
+  function addjobs(data,value){
     var card = data.map(function(element,index){
-        console.log(element.organizations[0].name)
-        return (`<div class='col-sm'><div class='card' style='width: 18rem;'> <img src='${element.organizations[0].picture}' class='card-img-top' alt=''...'> <div class='card-body'> <h5 class='card-title'>${element.organizations[0].name}</h5>  <p class='card-text'>Status : ${element.status}</p>    </div>   </div></div>`);
+      
+        if((element.organizations[0].name == value) || (element.objective == value)){
+        let picture = element.organizations[0].picture != null ? element.organizations[0].picture : 'https://via.placeholder.com/300';
+        let salary = element.compensation == null ?  'Private' : `<span>Min </span>${element.compensation.minAmount} - <span>Max </span>${element.compensation.maxAmount} ${element.compensation.currency}<span>/</span>${element.compensation.periodicity}`
         
+        return (`<div class='col-sm'>
+        <div class='card' style='width: 18rem;'> 
+        <img src='${picture}' class='card-img-top' alt='${element.organizations[0].name}'>
+         <div class='card-body'>
+          <h5 class='card-title'><a href=${element.id}>${element.objective}</a></h5>
+          </div>
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item"><strong>Company :</strong>${element.organizations[0].name}</li>
+          <li class="list-group-item"><strong>Status :</strong> ${element.status}</li>
+          <li class="list-group-item"><strong>Salary :</strong> ${salary}</li>
+          </ul>
+          <div class="card-body text-right">
+          <a href="#" class="card-link">View details</a>
+        </div> 
+    
+ 
+        </div>   <br/></div>`);
+        }else if(!value){
+            let picture = element.organizations[0].picture != null ? element.organizations[0].picture : 'https://via.placeholder.com/300';
+            let salary = element.compensation == null ?  'Private' : `<span>Min </span>${element.compensation.minAmount} - <span>Max </span>${element.compensation.maxAmount} ${element.compensation.currency}<span>/</span>${element.compensation.periodicity}`
+            
+            return (`<div class='col-sm'>
+            <div class='card' style='width: 18rem;'> 
+            <img src='${picture}' class='card-img-top' alt='${element.organizations[0].name}'>
+             <div class='card-body'>
+              <h5 class='card-title'><a href="job.html/?id=${element.id}">${element.objective}</a></h5>
+              </div>
+              <ul class="list-group list-group-flush">
+              <li class="list-group-item"><strong>Company :</strong>${element.organizations[0].name}</li>
+              <li class="list-group-item"><strong>Status :</strong> ${element.status}</li>
+              <li class="list-group-item"><strong>Salary :</strong> ${salary}</li>
+              </ul>
+              <div class="card-body text-right">
+              <a href="job.html/?id=${element.id}" class="card-link">View details</a>
+            </div> 
+        
+     
+            </div>   <br/></div>`);
+        }else{
+            return ('<div></div>');
+        }
     })
+    
     document.getElementById('jobs').innerHTML = card;  
     
   }
